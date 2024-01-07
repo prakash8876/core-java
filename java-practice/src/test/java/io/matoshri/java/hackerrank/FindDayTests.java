2cc3f1a1f7ecc8725ce392705eddbbde79f404b4
Prakash <prakash8876@gmail.com>
2024-01-08 01:42:35 +0530

    Update FindDayTests.java

--- java-practice/src/test/java/io/matoshri/java/hackerrank/FindDayTests.java
+++ java-practice/src/test/java/io/matoshri/java/hackerrank/FindDayTests.java
@@ -1,16 +1,21 @@
 package io.matoshri.java.hackerrank;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 class FindDayTests {
-    @Test
-    void testFindDay() {
-        String expected = "";
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2017, 8, 14);
-        String day = String.valueOf(calendar.get(Calendar.DAY_OF_WEEK));
+  @Test
+  void testFindDay() {
+    String expected = "WEDNESDAY";
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(2017, 8 - 1, 14);
+    String displayName = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US);
+    Assertions.assertEquals(expected, displayName);
     }
 }
